2b1d4d73c8f51012289f308f3cc75296605acef9
Lexas <dislexas@gmail.com>
2014-04-01 21:58:25 -0700

    save path access for each domain

--- data/cfs-embed.js
+++ data/cfs-embed.js
@@ -1,27 +1,33 @@
 
 var emit = self.port.emit;
 var doc = document.documentElement;
-var path = null; //working path
 var actions = {
 	'get-tree': function(data){
 		emit('get-tree', data);
+		return;
 	},
 	open: function(data){
 		emit('open-file', data.dir);
-		return 0;
+		return;
+	},
+	'revoke': function(data){
+		emit('revoke');
+		return;
 	},
 	'save-file': function(data){
 		data.path = path;
 		emit('save-file', data);
+		return;
 	},
 	'set-path': function(data){
-		emit('prompt-file', data);
+		emit('set-path', data);
+		return;
 	},
 };
 
 var cfs = function(action, data){
 	actions[action](data);
-	return 1;
+	return;
 };
 
 
@@ -34,25 +40,42 @@ doc.dispatchEvent(oncfs);
 doc.addEventListener('cfs-ask', function ask(ev){
 	doc.dispatchEvent(oncfs);
 	doc.removeEventListener('cfs-ask', ask);
-	return false;
+	return;
 });
 
+
+/* ====================== Response Events ============================= */
+
+var fileSaved = new CustomEvent('file-saved');
+var fileCreated = new CustomEvent('file-created');
 
 self.port.on('create-on-path', function(data){
-
+	return;
 });
-
+self.port.on('file', function(data){
+	var gotTree = new CustomEvent('got-file', {detail: data});
+	doc.dispatchEvent(gotFile);
+	return;
+});
 self.port.on('files', function(data){
+	var gotTree = new CustomEvent('got-files', {detail: data});
+	doc.dispatchEvent(gotFiles);
+	return;
 });
 self.port.on('save-on-path', function(data){
-
+	doc.dispatchEvent(fileSaved);
+	return;
 });
-self.port.on('set-path', function(data){
-	path = data;
-	var pathSet = new CustomEvent('path-set');
-	doc.dispatchEvent(pathSet);
+self.port.on('path-set', function(data){
+	var pathSet = new CustomEvent('path-set', {detail: data});
+	return;
 });
 self.port.on('got-tree', function(data){
 	var gotTree = new CustomEvent('got-tree', {detail: data});
 	doc.dispatchEvent(gotTree);
+	return;
 });
+self.port.on('error', function(data){
+	var error = new CustomEvent('cfs-error', {detail:data});
+	return;
+})
