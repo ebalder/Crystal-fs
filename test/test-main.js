efb353d6cd6d987598e1f5d1c54e73850e19d3cd
Lexas <dislexas@gmail.com>
2014-04-10 21:17:07 -0600

    IndexedDB pathKey recovery + documentation

--- test/test-main.js
+++ test/test-main.js
@@ -1,12 +1,22 @@
 var main = require("./main");
 
+worker = {
+	emit: function(){},
+	url: 'http://test.com/'
+};
+var paths = [];
+
 exports["test main"] = function(assert) {
   assert.pass("Unit test running!");
 };
 
-exports["test main async"] = function(assert, done) {
-  assert.pass("async Unit test running!");
-  done();
+exports["test get tree"] = function(assert, done) {
+	var data = {
+		limit: 3,
+		restore: paths[paths.length -1]
+	}
+	assert.pass("async Unit test running!");
+	done();
 };
 
 require("sdk/test").run(exports);
